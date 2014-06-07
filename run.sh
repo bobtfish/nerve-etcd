b34c5a755bf56eecabc38d7b1a49f604f09e5359
Tomas Doran <tdoran@yelp.com>
2014-06-07 14:58:46 +0100

    Fix

--- run.sh
+++ run.sh
@@ -17,7 +17,7 @@ if [ $PINGSTATUS != 0 ];then
 fi
 
 sed -i -e"s/%%INSTANCE_ID%%/${NERVE_INSTANCE}/" /nerve.conf.json
-env|egrep '\w+_PORT=tcp://'|sed -e's/\(.*\)_PORT=tcp:../\1:/'|ruby1.9.3 -e'require "json";STDIN.each_line {|l| d=l.chomp.split(/:/); File.open("/nerve_services/#{d[0]}.json", "w") {|f| f.puts Hash["host",d[1],"port",d[2],"reporter_type","etcd","etcd_host",ENV["ETCD_PORT_4001_TCP_ADDR"],"etcd_port",ENV["ETCD_PORT_4001_TCP_PORT"].to_i,"etcd_path","/nerve/services/#{ENV[\"NERVE_APP\"]}/services","check_interval",2,"checks",[Hash["type","tcp","timeout",0.2,"rise",3,"fall",2]]].to_json}}'
+env|egrep '\w+_PORT=tcp://'|sed -e's/\(.*\)_PORT=tcp:../\1:/'|ruby1.9.3 -e'require "json";NERVE_APP=ENV["NERVE_APP"];STDIN.each_line {|l| d=l.chomp.split(/:/); File.open("/nerve_services/#{d[0]}.json", "w") {|f| f.puts Hash["host",d[1],"port",d[2],"reporter_type","etcd","etcd_host",ENV["ETCD_PORT_4001_TCP_ADDR"],"etcd_port",ENV["ETCD_PORT_4001_TCP_PORT"].to_i,"etcd_path","/nerve/services/#{NERVE_APP}/services","check_interval",2,"checks",[Hash["type","tcp","timeout",0.2,"rise",3,"fall",2]]].to_json}}'
 rm -f /nerve_services/ETCD.json
 
 SERVICECOUNT=$(ls /nerve_services | wc -l)
