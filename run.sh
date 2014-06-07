29e7aaa76256e3a59d9090f541c5dd54075b6943
Tomas Doran <tdoran@yelp.com>
2014-06-07 14:39:40 +0100

    Actually use service name + better usage error reporting

--- run.sh
+++ run.sh
@@ -8,18 +8,28 @@ if [ "$NERVE_INSTANCE" == "" ];then
   exit 1
 fi
 PINGOUT=$(ping -c 1 -i 0 etcd 2>&1);
-if [ $? != 0 ];then
+PINGSTATUS=$?
+if [ $PINGSTATUS != 0 ];then
   echo "WARNING: Cannot find host named 'etcd', you need to link an etcd container to this container!" >&2
   echo "$PINGOUT" >&2
-#  exit 2
+  exit 2
 fi
 
 sed -i -e"s/%%NERVE_INSTANCE%%/${NERVE_INSTANCE}/" /nerve.conf.json
-env|egrep '\w+_PORT=tcp://'|sed -e's/\(.*\)_PORT=tcp:../\1:/'|ruby1.9.3 -e'require "json";STDIN.each_line {|l| d=l.chomp.split(/:/); File.open("/nerve_services/#{d[0]}.json", "w") {|f| f.puts Hash["host",d[1],"port",d[2],"reporter_type","etcd","etcd_host",ENV["ETCD_PORT_4001_TCP_ADDR"],"etcd_port",ENV["ETCD_PORT_4001_TCP_PORT"].to_i,"etcd_path","/nerve/services/your_service_name/services","check_interval",2,"checks",[Hash["type","tcp","timeout",0.2,"rise",3,"fall",2]]].to_json}}'
-rm /nerve_services/ETCD.json
+env|egrep '\w+_PORT=tcp://'|sed -e's/\(.*\)_PORT=tcp:../\1:/'|ruby1.9.3 -e'require "json";STDIN.each_line {|l| d=l.chomp.split(/:/); File.open("/nerve_services/#{d[0]}.json", "w") {|f| f.puts Hash["host",d[1],"port",d[2],"reporter_type","etcd","etcd_host",ENV["ETCD_PORT_4001_TCP_ADDR"],"etcd_port",ENV["ETCD_PORT_4001_TCP_PORT"].to_i,"etcd_path","/nerve/services/#{ENV[\"NERVE_APP\"]}/services","check_interval",2,"checks",[Hash["type","tcp","timeout",0.2,"rise",3,"fall",2]]].to_json}}'
+rm -f /nerve_services/ETCD.json
+
+SERVICECOUNT=$(ls /nerve_services | wc -l)
+if [ $SERVICECOUNT == 0 ];then
+  echo "WARNING: No services found, did you link any containers to this one?"
+fi
 
 # Default argument
 if [ "$1" == "run" ];then
+  if [ $PINGSTATUS != 0 ];then
+    echo "Shelling out as setup failed"
+    exec /bin/bash
+  fi
   exec /usr/local/bin/nerve -c /nerve.conf.json
 fi
 
